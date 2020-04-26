0d4572962d0e89081f1885618d86ff9c7141b9ad
bimsaraderaniyagala <bimsaraderaniyagala001@gmail.com>
2020-04-26 23:35:00 +0530

    Updates

---
+++ precache-manifest.99f30a2978cfe096d82418414cc28d55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a52479e0c6a2c47caacf87c839022f8",
+    "url": "/movieInfo/index.html"
+  },
+  {
+    "revision": "6d75691fb672fe672bf3",
+    "url": "/movieInfo/static/css/main.bb6342d4.chunk.css"
+  },
+  {
+    "revision": "af74045f63b6c9eb500e",
+    "url": "/movieInfo/static/js/2.43bdf12e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movieInfo/static/js/2.43bdf12e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d75691fb672fe672bf3",
+    "url": "/movieInfo/static/js/main.babc7fd1.chunk.js"
+  },
+  {
+    "revision": "5e1b38f3d556149ec292",
+    "url": "/movieInfo/static/js/runtime-main.b993ccae.js"
+  }
+]);
